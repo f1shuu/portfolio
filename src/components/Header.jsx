c567bba53e9904f485259dd3cc3c9c525adbbcfe
f1shu <szulzyckifilip@gmail.com>
2024-01-23 14:52:02 +0100

    Update landing page design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,17 +19,17 @@ const buttonsData = [
 export default function Header() {
     return (
         <>
-            <div className='flex lg:justify-end justify-center items-center w-[100vw] lg:h-[10vh] md:h-[10vh] sm:h-[10vh] bg-[#F0F6F6] lg:px-20 md:px-10 sm:px-6 fixed shadow-lg'>
+            <div className='flex lg:justify-end justify-center items-center w-[100vw] lg:h-[10vh] md:h-[10vh] sm:h-[10vh] bg-[#4BB3FD] lg:px-20 md:px-10 sm:px-6 fixed shadow-lg z-40'>
                 {buttonsData.map((element, index) => (
                     <button key={index} className='inline-flex items-center whitespace-nowrap relative border-none transition ease-in-out overflow-hidden lg:px-5 md:px-4 sm:px-3 px-2 py-3 group'>
                         <Link to={element.link} spy={true} smooth={true} duration={500} className='cursor-pointer breeze-bold lg:text-xl md:text-lg sm:text-base text-xs text-[#2A2D34]'>{element.text}</Link>
-                        <span className='absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-1 bg-[#376DEC] transition-all duration-500 ease-in-out group-hover:w-full' />
+                        <span className='absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-1 bg-[#2A2D34] transition-all duration-500 ease-in-out group-hover:w-full' />
                     </button>
                 ))}
                 <button onClick={handleDownloadCV} className='inline-flex items-center whitespace-nowrap relative border-none transition ease-in-out overflow-hidden lg:px-5 md:px-4 sm:px-3 px-2 py-3 group'>
                     <span className='cursor-pointer breeze-bold lg:text-xl md:text-lg sm:text-base text-xs text-[#2A2D34]'>Download CV</span>
                     <AiFillFilePdf className='ml-2 lg:text-3xl md:text-3xl sm:text-2xl text-lg text-[#2A2D34]' />
-                    <span className='absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-1 bg-[#376DEC] transition-all duration-500 ease-in-out group-hover:w-full' />
+                    <span className='absolute left-1/2 transform -translate-x-1/2 bottom-0 w-0 h-1 bg-[#4BB3FD] transition-all duration-500 ease-in-out group-hover:w-full' />
                 </button>
             </div>
         </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-scroll';
 import { AiOutlineArrowDown, AiOutlineMail, AiOutlineLinkedin, AiOutlineGithub } from 'react-icons/ai';
 import '../fonts.css';
+import circuit from '../images/circuit.png';
 
 const rotatingTexts = [
     'computer science student',
@@ -16,9 +17,9 @@ const handleEmailClick = () => {
 export default function Home() {
     return (
         <>
-            <div id='#home' className='bg-gradient-to-r from-[#000] to-[#376DEC] h-[100vh] px-[15%] text-left'>
-                <div className='h-[100%] flex flex-col items-center justify-center'>
-                    <span className='type-hello lg:text-6xl md:text-6xl sm:text-5xl text-3xl font-mono text-[#F0F6F6]' />
+            <div id='#home' className='bg-[#212121] h-[100vh] text-left'>
+                <div className='h-[75%] px-[15%] flex flex-col justify-center items-center'>
+                    <span className='type-hello lg:text-6xl md:text-6xl sm:text-5xl text-3xl font-mono text-[#4BB3FD]' />
                     <div className='wrapper flex breeze-regular md:h-10 h-8 lg:text-3xl md:text-3xl sm:text-2xl text-xl lg:my-10 md:my-10 sm:my-5 my-3'>
                         <div className='texts'>
                             {rotatingTexts.map((text, index) => (
@@ -29,21 +30,25 @@ export default function Home() {
                         </div>
                     </div>
                     <div className='cursor-pointer flex lg:mb-10 md:mb-10 sm:mb-5 mb-3 lg:text-4xl md:text-4xl sm:text-2xl text-xl text-[#F0F6F6]'>
-                        <AiOutlineMail onClick={handleEmailClick} className='mx-1' />
-                        <a href='https://linkedin.com/in/fszulzycki/' className='mx-1'>
+                        <AiOutlineMail onClick={handleEmailClick} className='mx-1 hover:text-[#4BB3FD]' />
+                        <a href='https://linkedin.com/in/fszulzycki/' className='mx-1 hover:text-[#4BB3FD]'>
                             <AiOutlineLinkedin />
                         </a>
-                        <a href='https://github.com/f1shuu' className='mx-1'>
+                        <a href='https://github.com/f1shuu' className='mx-1 hover:text-[#4BB3FD]'>
                             <AiOutlineGithub />
                         </a>
                     </div>
-                    <Link to='#contact' smooth={true} duration={500} className='link cursor-pointer flex justify-center overflow-hidden border-[#F0F6F6] border-2 rounded-full breeze-bold lg:text-lg md:text-base text-[#F0F6F6] hover:bg-[#F0F6F6] hover:text-[#2A2D34] active:bg-[#F0F6F6] active:border-[#F0F6F6] active:text-[#2A2D34] lg:px-8 lg:py-4 md:px-8 md:py-4 px-4 pt-2 pb-1 text-xs uppercase'>
+                    <Link to='#portfolio' smooth={true} duration={500} className='link cursor-pointer flex justify-center overflow-hidden border-[#F0F6F6] border-2 rounded-full breeze-bold lg:text-lg md:text-base text-[#F0F6F6] hover:bg-[#4BB3FD] hover:border-[#4BB3FD] hover:text-[#2A2D34] active:bg-[#4BB3FD] active:border-[#4BB3FD] active:text-[#2A2D34] lg:px-8 lg:py-4 md:px-8 md:py-4 px-4 pt-2 pb-1 text-xs uppercase'>
                         Learn more
                         <div className='arrows inline-block'>
                             <AiOutlineArrowDown className='upper-arrow ml-2 lg:text-2xl md:text-xl sm:text-lg text-base' />
                             <AiOutlineArrowDown className='lower-arrow ml-2 lg:text-2xl md:text-xl sm:text-lg text-base' />
                         </div>
                     </Link>
+                </div>
+                <div className='flex flex-row justify-between mb-20'>
+                    <img src={circuit} />
+                    <img src={circuit} className='transform scale-x-[-1] z-10' />
                 </div>
             </div>
         </>
